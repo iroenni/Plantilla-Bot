e0f454bd77da031908d7b89e1eb0eaf56977e9ff
Iro/dev <iroennis2005+40@gmail.com>
2025-10-18 08:32:53 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 #Conectar bot con el cliente
 app = Client(
 "bot",
-api_id="", #Ingresa tu api_id
-api_hash="", #Ingresa tu api_hash
-bot_token="") #Ingresa el token de tu bot
+api_id=14681595, #Ingresa tu api_id
+api_hash="a86730aab5c59953c424abb4396d32d5", #Ingresa tu api_hash
+bot_token="8138537409:AAHGgzcTdoKEPQlMhbfjAVJuWkX8-M7s_wo") #Ingresa el token de tu bot
  
 @app.on_message()
 def commands(client, message):
